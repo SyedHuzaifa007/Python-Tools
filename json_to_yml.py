a4657b9815633f39f093fc0ea185440b47a1bdb4
Syed Huzaifa <huzaifanau123@gmail.com>
2021-06-25 23:45:59 +0500

    Updated json_yml.py

--- json_to_yml.py
+++ json_to_yml.py
@@ -16,3 +16,15 @@ else:
     print("Usage: json_to_yaml.py <source_file.json> [target_file.yaml]")
 
 output = yaml.dump(source_content)
+
+if len(sys.argv) < 3:
+    print(output)
+
+elif os.path.exists(sys.argv[2]):
+    print("Sorry... " + sys.argv[2] + " Already Exists")
+    exit(1)
+
+else:
+    target_file = open(sys.argv[2], "w")
+    target_file.write(output)
+    target_file.close()
