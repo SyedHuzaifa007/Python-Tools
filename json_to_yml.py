fc688cfe23640532fc66153ed1f536b6a3f11bb8
Syed Huzaifa <huzaifanau123@gmail.com>
2021-06-25 23:25:02 +0500

    Update json_to_yml.py

--- json_to_yml.py
+++ json_to_yml.py
@@ -2,3 +2,9 @@ import json
 import os
 import sys
 import yaml
+
+if len(sys.argv) > 1:
+    if os.path.exists(sys.argv[1]):
+        source_file = open(sys.argv[1], "r")
+        source_content = json.load(source_file)
+        
