67301b1c3e66614cc145d9336e1e4c5e6781e669
Syed Huzaifa <huzaifanau123@gmail.com>
2021-06-25 23:38:50 +0500

    Updated json_to_yml.py

--- json_to_yml.py
+++ json_to_yml.py
@@ -7,4 +7,8 @@ if len(sys.argv) > 1:
     if os.path.exists(sys.argv[1]):
         source_file = open(sys.argv[1], "r")
         source_content = json.load(source_file)
-        
+        source_file.close()
+
+    else:
+        print("Sorry... " + sys.argv[1] + "Not Found")
+        exit(1)
